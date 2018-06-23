6f28b980adb65635098aa0042232bc1c74fb8dce
fang.yuan <yf381966217@163.com>
2018-06-23 11:40:04 +0800

    1. init vue backend 2. adjust login/logout module

---
+++ src/api/login.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: '/adminLogin',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo(token) {
+  console.log('getInfo', token)
+  return request({
+    url: '/userinfo',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
